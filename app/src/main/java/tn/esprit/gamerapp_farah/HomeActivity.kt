69df03ce79a1206bfaafc010cb77d6403bf086d1
Ranim-Rabah <79060004+Farah-Torkhani@users.noreply.github.com>
2023-10-15 23:33:23 +0100

    adding menu and toolbar layout and trying to bind them

--- app/src/main/java/tn/esprit/gamerapp_farah/HomeActivity.kt
+++ app/src/main/java/tn/esprit/gamerapp_farah/HomeActivity.kt
@@ -2,6 +2,7 @@ package tn.esprit.gamerapp_farah
 
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
@@ -11,7 +12,7 @@ class HomeActivity : AppCompatActivity() {
 
 private  lateinit var fragmentManager: FragmentManager
 private lateinit var binding: HomeActivityBinding
-
+private lateinit var binding1: HomeActivity
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,13 +28,22 @@ binding.bt1.setOnClickListener{
             goToFragment(Store())
         }
 
+
+
     }
+
 
 
     private  fun goToFragment(fragment: Fragment){
         fragmentManager = supportFragmentManager
         fragmentManager.beginTransaction().replace(R.id.fragmentContainer,fragment).commit()
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.custom_menu, menu) // Inflate your custom menu XML
+        return true
+    }
+
 
 
 }
